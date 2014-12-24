38b9b6c42f22f9753c77dc6a71c631b8474ca202
Raphael Lehmann <raphael@rleh.de>
2014-12-24 16:53:53 +0100

    Cronjob: delete expired tokens

--- api1/cron.php
+++ api1/cron.php
@@ -6,7 +6,8 @@ $db = new mysqli($dbhost, $dbuser, $dbpass);
 if($db->connect_error) die('Datenbankverbindung nicht möglich. ');
 $db->select_db($dbname);
 
-
-
+// Remove expired tokens from database:
+$db->query("DELETE FROM `ibis_server-php`.`tokens` WHERE `expired`>".time());
+		
 $db->close();
 ?>
