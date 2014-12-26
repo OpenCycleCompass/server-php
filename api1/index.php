6517ab8e9bde52c89b46ec39cb1a99fc60093a9f
Raphael Lehmann <raphael@rleh.de>
2014-12-26 01:36:54 +0100

    PostgreSQL database connection

--- api1/index.php
+++ api1/index.php
@@ -1,20 +1,35 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-date_default_timezone_set('Europe/Berlin');
-include('config.php');
-$err_level = error_reporting(0);
-$my = new mysqli($my_host, $my_user, $my_pass);
-error_reporting($err_level);
-if($my->connect_error) die("Datenbankverbindung nicht möglich.");
-$my->set_charset('utf8');
-$my->select_db($my_name);
+header ( 'Content-Type: text/html; charset=utf-8' );
+date_default_timezone_set ( 'Europe/Berlin' );
+include ('config.php');
+$err_level = error_reporting ( 0 );
+$my = new mysqli ( $my_host, $my_user, $my_pass );
+error_reporting ( $err_level );
+if ($my->connect_error)
+	die ( "Datenbankverbindung (MySQL) nicht möglich." );
+$my->set_charset ( 'utf8' );
+$my->select_db ( $my_name );
 
+$pg = pg_connect ( "host=" . $pg_host . " dbname=" . $pg_name . " user=" . $pg_user . " password=" . $pg_pass ) or die ( "Datenbankverbindung (MySQL) nicht möglich." . pg_last_error () );
 
-//$my->real_escape_string($_POST["text"]);
-//$query_text = "INSERT INTO `db`.`table` (`id`, `NAME`) VALUES (NULL, '".$whatever."')";
-//result = $my->query($query_text);
+// MySQL Example:
+// $my->real_escape_string($_POST["text"]);
+// $query_text = "INSERT INTO `db`.`table` (`id`, `NAME`) VALUES (NULL, '".$whatever."')";
+// result = $my->query($query_text);
 //
-//result->close();
+// result->close();
 
-$my->close();
+// PostgreSQL Example:
+// Eine SQL-Abfrge ausführen
+//$query = 'SELECT * FROM authors';
+//$result = pg_query ( $query ) or die ( 'Abfrage fehlgeschlagen: ' . pg_last_error () );
+//while ( $line = pg_fetch_array ( $result, null, PGSQL_ASSOC ) ) {
+//	foreach ( $line as $col_value ) {
+//		$x = $col_value;
+//	}
+//}
+//pg_free_result ( $result );
+
+pg_close ( $dbconn );
+$my->close ();
 ?>
