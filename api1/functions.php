eff063de3d84387f6bf42c7f6a28198356cbe847
Raphael Lehmann <raphael@rleh.de>
2014-12-28 01:38:44 +0100

    mysqli::affected_rows is a variable, not a function

--- api1/functions.php
+++ api1/functions.php
@@ -1,7 +1,7 @@
 <?php
 function verify_token($token, $my) {
 	$my->query("SELECT `id` FROM `ibis_server-php`.`tokens` WHERE `token`='".$token."'");
-	if($my->affected_rows() == 1) {
+	if($my->affected_rows == 1) {
 		return 1;
 	} else {
 		return 0;
