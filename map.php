aab63fe2e681f07e9569fce565e5a44949154725
Raphael Lehmann <rll.lehmann@gmail.com>
2015-01-05 21:09:34 +0100

    gettrack: array length JQuery util

--- map.php
+++ map.php
@@ -126,8 +126,7 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 			    [38.89340438498248, -77.03514575958252],
 			    [38.893596444352134, -77.0349633693695]
 			];
-			var line_points3 = new Array(line_points.length);
-			line_points3 = line_points;
+			var line_points3 = $.makeArray(line_points);
 			console.log(line_points2);
 			console.log(line_points3);
 			console.log(line_points);
