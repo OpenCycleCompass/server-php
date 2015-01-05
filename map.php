d2c5ab319608d51fbef44d93fb2e20feecf21acb
Raphael Lehmann <raphael@rleh.de>
2015-01-05 23:51:37 +0100

    Code cleanup

--- map.php
+++ map.php
@@ -36,12 +36,12 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 				<form id="show_track">
 					<select id="track_select">
 					<?php 
-					$query = "SELECT `name`,`track_id` FROM `ibis_server-php`.`tracks` LIMIT 10000;";
+					$query = "SELECT `name`,`track_id`, `nodes` FROM `ibis_server-php`.`tracks` LIMIT 10000;";
 					$result = $my->query($query);
 					if($result->num_rows >= 1){
 						$data = array();
 						while($row = $result->fetch_array()){
-							echo("\t\t\t\t\t\t<option value=\"" . $row["track_id"] . "\">" . $row["name"] . "</option>\n");
+							echo("\t\t\t\t\t\t<option value=\"" . $row["track_id"] . "\">" . $row["name"] . "  " . "(" . $row["nodes"] . " Punkte)</option>\n");
 						}
 					}
 					?>
@@ -74,44 +74,6 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 		src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
 	<script src="leaflet-sidebar-v2/leaflet-sidebar.min.js"></script>
 	<script type="text/javascript">
-		var map = L.map('map').setView([51.505, -0.09], 13);
-
-		//L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-		// http://{s}.tile.thunderforest.com/cycle (OpenCycleMap) ist leider nicht über https verfügbar
-		// -> leider MixedContent 
-		L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
-			maxZoom: 18,
-			//attribution: 'Map data &copy; <a href="http://opencyclemap.org">OpenStreetMap</a> contributors, ' +
-			//	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			//	'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			//id: 'examples.map-i875mjb7'
-		}).addTo(map);
-	
-		var sidebar = L.control.sidebar('sidebar').addTo(map);
-
-		var line_points = [
-		                   [38.893596444352134, -77.0381498336792],
-		                   [38.89337933372204, -77.03792452812195],
-		                   [38.89316222242831, -77.03761339187622],
-		                   [38.893028615148424, -77.03731298446655],
-		                   [38.892920059048464, -77.03691601753235],
-		                   [38.892903358095296, -77.03637957572937],
-		                   [38.89301191422077, -77.03592896461487],
-		                   [38.89316222242831, -77.03549981117249],
-		                   [38.89340438498248, -77.03514575958252],
-		                   [38.893596444352134, -77.0349633693695]
-		               ];
-        
-		
-		// create a red polyline from an array of LatLng points
-		var polyline = L.polyline(line_points, {color: 'red'}).addTo(map);
-
-		// zoom the map to the polyline
-		map.fitBounds(polyline.getBounds());
-	
-		var clickTyp = 0;
-		var popup_start = L.popup();
-		var popup_end = L.popup();
 		function onMapClick(e) {
 			if(clickTyp == 0){
 				$("#start_lat").val(e.latlng.lat);
@@ -133,15 +95,12 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 				clickTyp = 0;
 			}
 		}
-
-		map.on('click', onMapClick);
-	</script>
-	<script type="text/javascript">
-		$( "#show_track" ).submit(function( event ) {
+			
+		function drawPolyline(urlJsonData){
 			// Get points of selected track an show it on map
 			// Create array of lat,lon points
 			var line_points_t = [];
-			$.getJSON("api1/gettrack.php?gettrack=gettrack&track_id="+$("#track_select").val(), function (json) {
+			$.getJSON(urlJsonData, function (json) {
 				for (var i = 0; i < json.length; i++) {
 					//line_points.push([json[i].lat, json[i].lon, json[i].alt]);
 					line_points_t.push(L.latLng(parseFloat(json[i].lat), parseFloat(json[i].lon)));
@@ -154,12 +113,32 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 				console.log(line_points.length);
 
 				// create a red polyline from an array of LatLng points
-				var polyline = L.polyline(line_points, {color: 'green'}).addTo(map);
+				var polyline = L.polyline(line_points, {color: 'red'}).addTo(map);
 				// zoom the map to the polyline
 				map.fitBounds(polyline.getBounds());
 			});
-		});
+		}
+		var map = L.map('map').setView([51.505, -0.09], 13);
 
+		// http://{s}.tile.thunderforest.com/cycle (OpenCycleMap) ist leider nicht über https verfügbar
+		// -> leider MixedContent 
+		L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+			maxZoom: 18
+		}).addTo(map);
+	
+		var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+	
+		var clickTyp = 0;
+		var popup_start = L.popup();
+		var popup_end = L.popup();
+
+
+		map.on('click', onMapClick);
+
+		$( "#show_track" ).submit(function( event ) {
+			drawPolyline("api1/gettrack.php?gettrack=gettrack&track_id="+$("#track_select").val());
+		});
 	</script>
 <?php
 pg_close ( $pg );
