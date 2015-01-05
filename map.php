83f40ee018d00621855285db885235c960c85562
Raphael Lehmann <rll.lehmann@gmail.com>
2015-01-05 20:11:51 +0100

    gettrack: js array length

--- map.php
+++ map.php
@@ -112,12 +112,13 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 		$( "#show_track" ).submit(function( event ) {
 			// Get points of selected track an show it on map
 			var line_points = [];
-			$.getJSON("api1/gettrack.php?gettrack=gettrack&track_id="+$("#track_select").val(), function (json) {
+			line_points = $.getJSON("api1/gettrack.php?gettrack=gettrack&track_id="+$("#track_select").val(), function (json) {
+	        	line_points = new Array(json.length);
 		        for (var i = 0; i < json.length; i++) {
-		        	line_points = new Array(json.length);
 		            //line_points.push([json[i].lat, json[i].lon, json[i].alt]);
 		            line_points[i] = [parseFloat(json[i].lat), parseFloat(json[i].lon)];
 		        }
+		        return line_points;
 			});
 			// Create array of lat,lon points.
 			var line_points2 = [
