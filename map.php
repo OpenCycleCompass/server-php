7a57caa89adce02e2d5a45f4d52ea532430cb686
Raphael Lehmann <rll.lehmann@gmail.com>
2015-01-05 12:40:15 +0100

    Leaflet: draw polyline

--- map.php
+++ map.php
@@ -8,8 +8,9 @@
 	href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css" />
 </head>
 <body style="height: 100%;">
-	<div id="wrapper" style="min-height: 100%; padding:0px; margin:0px;">
-		<div id="map" style="position:absolute; top:0; left:0; width:100%; height:100%;"></div>
+	<div id="wrapper" style="min-height: 100%; padding: 0px; margin: 0px;">
+		<div id="map"
+			style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></div>
 	</div>
 	<script
 		src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
@@ -28,7 +29,7 @@
 		}).addTo(map);
 	
 	
-		L.marker([51.5, -0.09]).addTo(map)
+		/*L.marker([51.5, -0.09]).addTo(map)
 			.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 	
 		L.circle([51.508, -0.11], 500, {
@@ -41,11 +42,30 @@
 			[51.509, -0.08],
 			[51.503, -0.06],
 			[51.51, -0.047]
-		]).addTo(map).bindPopup("I am a polygon.");
-	
+		]).addTo(map).bindPopup("I am a polygon."); */
+
+
+		var line_points = [
+		                   [38.893596444352134, -77.0381498336792],
+		                   [38.89337933372204, -77.03792452812195],
+		                   [38.89316222242831, -77.03761339187622],
+		                   [38.893028615148424, -77.03731298446655],
+		                   [38.892920059048464, -77.03691601753235],
+		                   [38.892903358095296, -77.03637957572937],
+		                   [38.89301191422077, -77.03592896461487],
+		                   [38.89316222242831, -77.03549981117249],
+		                   [38.89340438498248, -77.03514575958252],
+		                   [38.893596444352134, -77.0349633693695]
+		               ];
+        
+		
+		// create a red polyline from an array of LatLng points
+		var polyline = L.polyline(line_points, {color: 'red'}).addTo(map);
+
+		// zoom the map to the polyline
+		map.fitBounds(polyline.getBounds());
 	
 		var popup = L.popup();
-	
 		function onMapClick(e) {
 			popup
 				.setLatLng(e.latlng)
