a712a62caa097e8a027fa74b40d2af44ed0f31e0
Raphael Lehmann <raphael@rleh.de>
2015-01-11 17:22:04 +0100

    load tracklist: bugfix (JS)

--- map.php
+++ map.php
@@ -96,7 +96,7 @@ $pg = pg_connect ( $pg_connectstr ) or die ( "Datenbankverbindung (PostgreSQL) n
 	<script type="text/javascript" src="leaflet-sidebar-v2/leaflet-sidebar.min.js"></script>
 	<script type="text/javascript">
 		$( document ).ready(function() {
-			setTrackSelectOptions($("#track_select_num select"));
+			setTrackSelectOptions($("#track_select_num").val());
 		});
 
 		function setTrackSelectOptions(num) {
