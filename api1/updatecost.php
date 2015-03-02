f72d3dd24de71f6c5e1c3af8c6e48e81ff81c92e
Raphael Lehmann <raphael@rleh.de>
2015-03-02 02:08:49 +0100

    added leaflet sidebar pane to edit static cost; implemented cost profiles; added backend to edit cost profiles; modified gettopo.php and showtopo_pane to display each cost profile; fixed #24

---
+++ api1/updatecost.php
@@ -0,0 +1,48 @@
+<?php
+header ( 'Content-Type: text/html; charset=utf-8' );
+date_default_timezone_set ( 'Europe/Berlin' );
+include ('config.php');
+include ('functions.php');
+
+$pg = pg_connect($pgr_connectstr);
+if(!$pg)
+	die("Datenbankverbindung (PostgreSQL) nicht möglich.".pg_last_error($pg));
+
+if(isset($_GET["profile"])) {
+	$query = "SELECT id FROM classes WHERE profile = '".pg_escape_string($_GET["profile"])."';";
+	$result = pg_query($query);
+	if($result) {
+		$error_cnt = 0;
+		$query2 = "";
+		while($row = pg_fetch_assoc($result)){
+			if(isset($_GET[$row["id"]])) {
+				$val = floatval($_GET[$row["id"]]);
+			}
+			else {
+				$val = 1;
+				$error_cnt++;
+			}
+			$query2 .= "UPDATE classes SET cost = ".$val." WHERE profile = '".pg_escape_string($_GET["profile"])."' AND id = ".intval($row["id"]).";\n";
+		}
+		pg_free_result($result);
+		$result = pg_query($pg, $query2);
+		//echo($query2);
+		if($result) {
+			$out = json_encode(array("success" => "Profile ".$_GET["profile"]." successfully updated. (".$error_cnt." Fehler)"));
+		}
+		else {
+			die(json_encode(array("error" => "Profile ".$_GET["profile"]." was not updated. ".pg_last_error($pg))));
+		}
+	}
+	else {
+		die(json_encode(array("error" => "Profile not found or corrupted. ".pg_last_error($pg))));
+	}
+	
+} else {
+	$out = json_encode(array("error" => "Keine oder falsche Eingabe."));
+}
+
+echo ($out);
+
+pg_close ( $pgr );
+?>
