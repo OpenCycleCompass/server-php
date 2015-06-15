c96e243628ad5a138c8a5c1e467cc5f2c365a842
Raphael Lehmann <raphael@rleh.de>
2015-06-15 23:49:27 +0200

    processtrack: require authenticated user

--- api1/processtrack.php
+++ api1/processtrack.php
@@ -8,36 +8,36 @@ include('functions.php');
 $pg = pg_connect($pgr_connectstr);
 if(!$pg) die(json_encode(array("error" => "Database (PostgreSQL) failed." . pg_last_error())));
 
+session_start();
 
 include('../classes/processTracks.class.php');
 $processTracks = new processTracks($pg);
-
-if(isset($_GET['track_id'])) {
-	$out = array_merge($processTracks->processTrack(pg_escape_string($pg, $_GET['track_id'])), array("track_id" => $_GET['track_id']));
-	$out["executiontime"] = (microtime(true)-$start_microtime);
-	$out_r = json_encode($out);
-}
-else if(isset($_GET['all'])) {
-	$out = $processTracks->processAllTracks();
-	$out["executiontime"] = (microtime(true)-$start_microtime);
-	$out_r = json_encode($out);
-}
-else if(isset($_GET['clear'])) {
-	$out = $processTracks->deleteDynCosts();
-	$out["executiontime"] = (microtime(true)-$start_microtime);
-	$out_r = json_encode($out);
-}
-else if(isset($_GET['numseg'])) {
-	$out = $processTracks->getDynCostNumSeg();
-	$out["executiontime"] = (microtime(true)-$start_microtime);
-	$out_r = json_encode($out);
+if(isset($_SESSION["auth_user"])) {
+	if(isset($_GET['track_id'])) {
+		$out = array_merge($processTracks->processTrack(pg_escape_string($pg, $_GET['track_id'])), array("track_id" => $_GET['track_id']));
+		$out["executiontime"] = (microtime(true)-$start_microtime);
+	}
+	else if(isset($_GET['all'])) {
+		$out = $processTracks->processAllTracks();
+		$out["executiontime"] = (microtime(true)-$start_microtime);
+	}
+	else if(isset($_GET['clear'])) {
+		$out = $processTracks->deleteDynCosts();
+		$out["executiontime"] = (microtime(true)-$start_microtime);
+	}
+	else if(isset($_GET['numseg'])) {
+		$out = $processTracks->getDynCostNumSeg();
+		$out["executiontime"] = (microtime(true)-$start_microtime);
+	}
+	else {
+		$out = array("error" => "Eingaben fehlerhaft.");
+	}
 }
 else {
-	$out = array("error" => "Eingaben fehlerhaft.");
-	$out_r = json_encode($out);
+	$out = array("error" => "Not permitted. Please sign in.");
 }
 
-echo($out_r);
+echo(json_encode($out));
 
 pg_close($pg);
 ?>
